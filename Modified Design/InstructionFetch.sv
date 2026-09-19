a9a2bdac547cdb198fb78a3c220b5b70b9ab57f8
Spandan Mondal <spandanmondal0000@gmail.com>
2026-09-19 19:24:09 +0530

    Update InstructionFetch.sv

--- Modified Design/InstructionFetch.sv
+++ Modified Design/InstructionFetch.sv
@@ -5,29 +5,31 @@ module InstructionFetch(
     input  logic EN,
     input  logic Reset,
     input  logic PCWrite,
-    input  logic [31:0] PC_in,
+    
+    input  logic [31:0] NextPC,
 
-    output logic [31:0] Instruction,
-    output logic [31:0] PC
+    output logic [31:0] IF_Instruction,
+    output logic [31:0] IF_PC
 );
+    
+    logic [31:0] PC_reg;
+    
+    PC PCUnit(
+        .CLK(CLK),
+        .Reset(Reset),
+        .PCWrite(PCWrite),
+        .NextPC(NextPC),
+        .PC_out(PC_reg)
+    );
+    
+    InstructionMemory IM(
+        .EN(EN),
+        .InstructionAddress(PC_reg),
+        .ReadInstruction(IF_Instruction)
+    );
+    
+    assign IF_PC = PC_reg;
 
-logic [31:0] PC_reg;
-logic [31:0] InstrMem [0:255];
 
-// ? HARD-CODED FILE (SYNTHESIZABLE STYLE)
-initial begin
-    $readmemh("Instructions.mem", InstrMem);
-end
-
-// PC update
-always_ff @(posedge CLK or posedge Reset) begin
-    if (Reset)
-        PC_reg <= 0;
-    else if (EN && PCWrite)
-        PC_reg <= PC_in;
-end
-
-assign PC = PC_reg;
-assign Instruction = InstrMem[PC_reg[9:2]];
 
 endmodule
